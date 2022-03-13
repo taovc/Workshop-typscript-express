117d9f24b1c5897d855d6f23a335ce6792da1721
taovc <tao.weijie@epitech.eu>
2022-03-14 00:00:27 +0100

    [add] middleware.ts

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,8 @@ app.get("/hello", (_req, res, _next) => {
 });
 
 //
-// exercise 2: make a '/message' endpoint with get methode that display the message argument, return 200 if have not message return 400 and "bad request"
+// exercise 2: make a '/message' endpoint with get methode that display the message argument
+// return 200 if have not message return 400 and "bad request"
 //
 
 app.get("/message", (_req, res, _next) => {
@@ -19,7 +20,8 @@ app.get("/message", (_req, res, _next) => {
 });
 
 //
-// exercise 3: make a '/header' endpoint with get methode that If the request contains a `message` header, return 200 and message if not return 400 and "bad request"
+// exercise 3: make a '/header' endpoint with get methode that If the request contains a `message` header
+// return 200 and message if not return 400 and "bad request"
 //
 
 app.get('/header', (_req, res, _next) => {
@@ -27,26 +29,30 @@ app.get('/header', (_req, res, _next) => {
 });
 
 //
-// exercise 4: make a '/body' endpoint with get methode that If the request contains a `message` body, return 200 and message if not return 400 and "bad request"
+// exercise 4: make a '/body' endpoint with get methode that If the request contains a `message` body
+// return 200 and message if not return 400 and "bad request"
 //
-
-var bodyParser = require('body-parser')
-app.use(bodyParser.json())
-app.use(express.urlencoded({ extended: true }))
 
 app.post('/body', (_req, res, _next) => {
     
 });
 
 //
-// exercise 5: make a '/cookie' endpoint with get methode that If the request contains a `message` cookie, return 200 and message if not return 400 and "bad request"
+// exercise 5: make a '/cookie' endpoint with get methode that If the request contains a `message` cookie
+// return 200 and message if not return 400 and "bad request"
 //
-
-var cookieParser = require('cookie-parser');
-app.use(cookieParser())
 
 app.get('/cookie', (_req, res, _next) => {
     
+});
+
+//
+// exercise 6: make a '/private' endpoint with get methode that If the request contains a `Authorization` header
+// return 200 and "Welcome to admin page" when the password is good if not return 400 and "bad request"
+//
+
+app.get('/private', (_req, res, _next) => {
+
 });
 
 app.listen(3000, () => {
---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+//
+// exercise 6: use jsonwebtoken to create a token with the password
+//
+
+var key = {
+    "PRIVITE_KEY":"EPITECH",
+    "EXPIRESD":60*60*24
+}
+
+function createtoken() {
+
+}
+
+module.exports = {
+    createtoken
+}
